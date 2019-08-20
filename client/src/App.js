24f92a58a16a8ad0103d56417186d6d1c4f41fd8
Tai Nguyen <ntai9296@gmail.com>
2019-08-19 17:26:47 -0700

    react

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
@@ -10,14 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+	<Button>Semantic Button :)</Button>
       </header>
     </div>
   );
