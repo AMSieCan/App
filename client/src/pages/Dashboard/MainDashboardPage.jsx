607137a740ec91bcd1ae20e9cf34050328dcac61
rockycsumb <37380272+rockycsumb@users.noreply.github.com>
2019-09-14 13:04:23 -0700

    trying to add drawer

--- client/src/pages/Dashboard/MainDashboardPage.jsx
+++ client/src/pages/Dashboard/MainDashboardPage.jsx
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import GoogleMapReact from 'google-map-react';
+import Drawer from 'react-motion-drawer'
 
 
-class BinStatus extends React.Component {
-  render() {
-    
-    return (
-        <div>
-        <h1>Bin Stats</h1>
-        </div>
-      )
-  }
-}
+
+// you can use this library
+// https://github.com/stoeffel/react-motion-drawer
+// just do `yarn add react-motion-drawer`
+
+//then you can import the drawer into the comonent to use
+// you can add it in MainDashboardPage
+
+// then create a state to toggle the drawer
+
+// const [bin, setBin] = useState({});
+
+// Add a click event listener to your trash bin 
+// and trigger the setBin({ data here })
+// the drawer you can do somehting like 
+// <Drawer open={bin} onChange={(} => setBin(undefined)}>
+// so when drawer close you can reset the bin state
+
+
 
 
 
 const Pointer = (params) => {
-const handleClick = ()=> {
-      return (
-        console.log("bin stats")
-        )
-      }
-  
-
+  const [bin, setBin] = useState("empty");
 
   return (
     <div>
-      <div
-        role="tooltip"
-        x-placement="top"
-        style={{ top: '-77px', left: '-77px' }}
-        className="fade show popover bs-popover-top p-2">
-        
-        <i class="trash icon big">Bin</i>
-      </div>
-      <BinStatus />
+        <div
+          role="tooltip"
+          x-placement="top"
+          style={{ top: '-77px', left: '-77px' }}
+          className="fade show popover bs-popover-top p-2"
+          onClick={()=>setBin("trash full")}
+          >
+          <i class="trash icon big">Bin</i>
+        </div>
+
+        <h1>{bin}</h1>
+      
+        <div>
+          <Drawer open={bin} onChange={()=>setBin(undefined)}>
+            <ul>
+              <li>home</li>
+              <li>about</li>
+              <li>settings</li>
+            </ul>
+          </Drawer>
+        </div>
     </div>
 
   );
@@ -58,6 +74,7 @@ export default () => {
         <Pointer lat={36.653570} lng={-121.799310} />
         <Pointer lat={36.653368} lng={-121.798060} />
       </GoogleMapReact>
+      
     </div>
   );
 };
--- client/src/pages/Dashboard/binStats.jsx
+++ client/src/pages/Dashboard/binStats.jsx
@@ -1,15 +1,22 @@
+
 // you can use this library
 // https://github.com/stoeffel/react-motion-drawer
 // just do `yarn add react-motion-drawer`
+
 //then you can import the drawer into the comonent to use
 // you can add it in MainDashboardPage
+
 // then create a state to toggle the drawer
+
 // const [bin, setBin] = useState({});
+
 // Add a click event listener to your trash bin 
 // and trigger the setBin({ data here })
 // the drawer you can do somehting like 
 // <Drawer open={bin} onChange={(} => setBin(undefined)}>
 // so when drawer close you can reset the bin state
+
+
 
 
 function binStats() {
